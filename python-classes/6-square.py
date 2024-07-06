1640602376be8335c8df21ae52c6e76e431a3cfd
Ewing <e.singasara@alustudent.com>
2024-07-06 14:26:46 -0700

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -43,11 +43,11 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print("")
+            print()
             return
 
         for _ in range(self.__position[1]):
-            print("")
+            print()
 
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
