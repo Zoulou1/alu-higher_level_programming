31b3ebf9921500a364e7975d3fc544d087673973
Ewing <e.singasara@alustudent.com>
2024-07-06 13:44:20 -0700

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square.
+"""
+
+
+class Square:
+    """
+    This class represents a square with a private instance attribute size.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area.
+        """
+        return self.__size ** 2
