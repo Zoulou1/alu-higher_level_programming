44e07d42d69b202d0d1c23ff1834bf3e3a95c009
Ewing <e.singasara@alustudent.com>
2024-07-06 15:14:01 -0700

    0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
+
+
 class Rectangle:
-    pass
+    """
+    This class represents a rectangle.
+    """
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
