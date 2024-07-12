5f159958a85cbdf60638ff229d4d1a8a587b4e57
Ewing <e.singasara@alustudent.com>
2024-07-12 03:21:18 -0700

    100

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
+
+# Test cases
+if __name__ == "__main__":
+    import sys
+
+    value = 89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
