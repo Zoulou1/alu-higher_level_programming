1aee84b1c4619af814277b74b992f0775bff10b7
Ewing <e.singasara@alustudent.com>
2024-07-12 03:05:07 -0700

    101

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return s
+    return s[:n] + s[n+1:]
+
+if __name__ == "__main__":
+    print(remove_char_at("Best School", 3))  # Bes School
+    print(remove_char_at("Chicago", 2))      # Chcago
+    print(remove_char_at("C is fun!", 0))    #  is fun!
+    print(remove_char_at("School", 10))      # School
+    print(remove_char_at("Python", -2))      # Python
