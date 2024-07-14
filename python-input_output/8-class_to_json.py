bb438b05f0cd2ede7999805e42426e50b429b69d
Ewing <e.singasara@alustudent.com>
2024-07-14 14:44:52 -0700

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -13,14 +13,11 @@ def class_to_json(obj):
         dict: A dictionary representation of the object suitable for JSON
     """
     attributes = obj.__dict__.copy()  # Get instance attributes dictionary
-    
     # Handle private attributes (name mangled in Python)
     for key in attributes:
         if key.startswith("__") and not key.endswith("__"):
             attributes[key] = getattr(obj, key)
-    
     # Exclude methods and non-serializable attributes
     attributes = {key: val for key, val in attributes.items()
                   if isinstance(val, (list, dict, str, int, bool))}
-    
     return attributes
