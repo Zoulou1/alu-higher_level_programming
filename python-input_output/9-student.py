4dc029562c9c85f3cabc3ce22df573a6c85c128c
Ewing <e.singasara@alustudent.com>
2024-07-14 14:48:28 -0700

    9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 9-student
+Defines a Student class with attributes and methods for JSON serialization.
+"""
+
+
+class Student:
+    """A class representing a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance."""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
