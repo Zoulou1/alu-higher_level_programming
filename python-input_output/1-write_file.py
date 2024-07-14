135d63433cdfd428ce99374f364a094f8fef0dce
Ewing <e.singasara@alustudent.com>
2024-07-14 14:08:52 -0700

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 1-write_file
+Writes a string to a text file (UTF8) and returns the number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number
+    Args:
+        filename (str): The name of the file to write
+        text (str): The string to write to the file
+    
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
