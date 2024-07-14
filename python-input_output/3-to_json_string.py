02afdeeda3154593ac5ff83203fe56a04992d6c8
Ewing <e.singasara@alustudent.com>
2024-07-14 14:22:42 -0700

    3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: 3-to_json_string
+Returns the JSON representation of an object (string).
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string).
+    Args:
+        my_obj (any): The object to be serialized to JSON.
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
