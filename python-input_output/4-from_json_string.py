aaffa9cff2df1730726328ec5f82c4539be4c57b
Ewing <e.singasara@alustudent.com>
2024-07-14 14:24:56 -0700

    4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: 4-from_json_string
+Returns an object (Python data structure) represented by a JSON string.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string.
+    Args:
+        my_str (str): The JSON string.
+    Returns:
+        any: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
