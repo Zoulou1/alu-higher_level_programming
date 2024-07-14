aa0211fa4db284489662b00aa5106254488b9ea8
Ewing <e.singasara@alustudent.com>
2024-07-14 14:01:32 -0700

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: 0-read_file
+Reads a text file (UTF8) and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+    
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
