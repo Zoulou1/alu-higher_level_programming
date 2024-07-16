3453daeb726b3acd78df63782aeea2c7555d1660
Ewing <e.singasara@alustudent.com>
2024-07-16 10:25:03 -0700

    12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module: 12-pascal_triangle
+Defines a function that returns a list of lists of integers representing 
+Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n.
+    Args:
+        n (int): The number of rows of Pascal's triangle to generate.
+    Returns:
+        list: A list of lists of integers representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
+
+# Example usage
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
