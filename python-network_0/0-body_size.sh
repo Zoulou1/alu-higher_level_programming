e568f9a957f861ad9ccaadbe2ca7e856e241fda9
Ewing <e.singasara@alustudent.com>
2024-07-29 12:02:24 +0000

    0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL,
-# and displays the size of the body of the response in bytes.
-
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-URL=$1
-
-# Send a request to the URL and display the size of the response body in bytes
-curl -s -o /dev/null -w '%{size_download}\n' "$URL"
+# This script sends a request to the URL and displays the size of the body of the response in bytes.
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
