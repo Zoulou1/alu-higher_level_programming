cdcf3a89e626af9b2614a48f4783a8a8424723f5
Ewing <e.singasara@alustudent.com>
2024-07-30 09:32:39 +0000

    1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL and displays the body of a 200 status code response.
-curl -sL -o /dev/stderr -w "%{http_code}" "$1" 2> response_body | [ "$(tail -n 1 response_body)" = "200" ] && head -n -1 response_body
+#Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
