32c7ae5ea248b2c1d454f66576a7f80b4e0cb1e4
Ewing <e.singasara@alustudent.com>
2024-08-02 06:51:27 +0000

    4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends arguments using get
-curl -sG "$1" -H "X-School-User-Id: 98"
+#a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
