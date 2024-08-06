75b0fa91bb2aa0b845d4812d4901675d93260d07
Ewing <e.singasara@alustudent.com>
2024-08-06 14:23:29 +0000

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-# python script that takes your GitHub credentials (username and password),
-# and uses the 'GitHub API' to display your 'id'
+"""__summary__
+- Write a Python script that
+- fetches https://intranet.hbtn.io/status.
 """
-    use 'GitHub API to display 'id'
-"""
-import sys
 import requests
-from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
