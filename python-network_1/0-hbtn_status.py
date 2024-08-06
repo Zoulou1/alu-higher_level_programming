873b4fb20e0403e6f685f0afa40cbd3229750763
Ewing <e.singasara@alustudent.com>
2024-08-06 13:57:30 +0000

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
+urls = ["https://intranet.hbtn.io/status", "http://0.0.0.0:5050/status"]
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+for url in urls:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", body.decode('utf-8'))
+    print(f"Fetching {url}")
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
+    print()
